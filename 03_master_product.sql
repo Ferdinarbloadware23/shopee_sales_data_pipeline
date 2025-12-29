75782ce53727fb2bdd8976edec8a36d3fa8346fd
Ferdinarbloadware23 <ferdinarsatria23@gmail.com>
2025-12-30 01:44:44 +0700

    Create 03_master_product.sql

---
+++ 03_master_product.sql
@@ -0,0 +1,10 @@
+INSERT INTO master_product (
+    id_product,
+    product_name,
+    category,
+    brand,
+    cost_price,
+    sell_price
+)
+VALUES
+('TWS_LNV_XT60_BL', 'Lenovo XT60', 'TWS', 'Lenovo', 70000, 120000);
